c0a92af66ecc2f236174adef5c4746bcde72e070
Xiaocong (Rain) Yan <50472308+xcupsilon@users.noreply.github.com>
2022-02-18 00:15:36 -0500

    imports and now uses parcel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom";
+import App from "./App";
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const app = document.getElementById("app");
+ReactDOM.render(<App />, app);
